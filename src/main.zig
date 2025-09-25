29bd41b2612d2ad4ba49097585ae32670f2c2e6e
significant-cijfer <fake.potsjem@gmail.com>
2025-09-25 23:05:14 +0200

    Changed field repr, to reduce mem usage

--- src/main.zig
+++ src/main.zig
@@ -17,19 +17,19 @@ const Memo = struct {
         self.wins.deinit();
     }
 
-    fn hash(_: *Memo, key: []bool) u64 {
+    fn hash(_: *Memo, key: []u32) u64 {
         var hasher = std.hash.Wyhash.init(0);
         std.hash.autoHashStrat(&hasher, key, .Deep);
         return hasher.final();
     }
 
     fn remember(self: *Memo, field: Field, win: bool) !void {
-        const h = self.hash(field.cells);
+        const h = self.hash(field.alive);
         try self.wins.put(h, win);
     }
 
     fn recall(self: *Memo, field: Field) ?bool {
-        const h = self.hash(field.cells);
+        const h = self.hash(field.alive);
         return self.wins.get(h);
     }
 };
@@ -37,27 +37,28 @@ const Memo = struct {
 const Field = struct {
     wdt: u32,
     hgt: u32,
-    cells: []bool,
+    alive: []u32, // calculated from top to bottom, how many cells still on the field
 
     pub fn init(gpa: Allocator, width: u32, height: u32) !Field {
-        const cells = try gpa.alloc(bool, width*height);
-        @memset(cells, false);
+        const alive = try gpa.alloc(u32, width);
+        @memset(alive, height);
 
         return .{
             .wdt = width,
             .hgt = height,
-            .cells = cells,
+            .alive = alive,
         };
     }
 
     pub fn deinit(self: Field, gpa: Allocator) void {
-        gpa.free(self.cells);
+        gpa.free(self.alive);
     }
 
     pub fn print(self: Field) void {
         for (0..self.hgt) |h| {
             for (0..self.wdt) |w| {
-                std.debug.print("{d} ", .{@intFromBool(self.cells[w+h*self.wdt])}); //NOTE, print as either 0/1
+                const alive = self.alive[w];
+                std.debug.print("{d} ", .{@intFromBool(h < alive)}); //NOTE, print as either 0/1
             }
 
             std.debug.print("\n", .{});
@@ -68,18 +69,16 @@ const Field = struct {
         return .{
             .wdt = self.wdt,
             .hgt = self.hgt,
-            .cells = try gpa.dupe(bool, self.cells),
+            .alive = try gpa.dupe(u32, self.alive),
         };
     }
 
     fn eat(self: Field, x: u32, y: u32) void {
-        // oob check
-        if (x >= self.wdt or y >= self.hgt) @panic("TODO, figure out how tf this happened");
+        if (x >= self.wdt or y >= self.hgt) @panic("TODO, figure out how tf this happened"); // oob check
 
-        for (y..self.hgt) |h| {
-            for (x..self.wdt) |w| {
-                self.cells[w+h*self.wdt] = true;
-            }
+        for (x..self.wdt) |w| {
+            if (self.alive[w] > y)
+                self.alive[w] = y;
         }
     }
 
@@ -87,41 +86,52 @@ const Field = struct {
         self.eat(@intCast(idx % self.wdt), @intCast(idx / self.wdt));
     }
 
-    fn nextMove(self: Field, start: u64) ?u64 {
-        return indexOfScalarPos(bool, self.cells, start, false);
+    fn nextMove(self: Field, start: u64) !?u64 {
+        for (start..self.wdt*self.hgt) |pos| {
+            const x = pos % self.wdt;
+            const y = pos / self.wdt;
+
+            if (self.alive[x] > y) return pos;
+        }
+
+        return null;
     }
 
-    pub fn analyze(gpa: Allocator, memo: *Memo, width: u32, height: u32) !Field {
-        var wins = try Field.init(gpa, width, height);
+    pub fn analyze(gpa: Allocator, memo: *Memo, width: u32, height: u32) ![]bool {
+        const wins = try gpa.alloc(bool, width*height);
 
         for (0..width*height) |idx| {
             std.debug.print("{d} cells remaining\n", .{width*height-idx});
 
+            var timer = try std.time.Timer.start();
             var field = try Field.init(gpa, width, height);
             defer field.deinit(gpa);
 
             field.eatIdx(idx);
 
-            const win = try field.guaranteedWin(gpa, memo, false);
-            wins.cells[idx] = !win;
+            const win = try field.guaranteedWin(gpa, memo);
+            wins[idx] = !win;
+
+            const lap = timer.lap();
+            std.debug.print("took {}ns, {}ms, {}s\n\n", .{lap, lap/1000000, lap/1000000000});
         }
 
         return wins;
     }
 
-    fn guaranteedWin(self: *Field, gpa: Allocator, memo: *Memo, p1: bool) !bool {
+    fn guaranteedWin(self: *Field, gpa: Allocator, memo: *Memo) !bool {
         var start: u64 = 0;
 
-        if (self.nextMove(0) == null) return true;
+        if (try self.nextMove(0) == null) return true;
         if (memo.recall(self.*)) |w| return w;
 
-        while (self.nextMove(start)) |pos| : (start = pos+1) {
+        while (try self.nextMove(start)) |pos| : (start = pos+1) {
             var field = try self.clone(gpa);
             defer field.deinit(gpa);
 
             field.eatIdx(pos);
 
-            const w = try field.guaranteedWin(gpa, memo, !p1);
+            const w = try field.guaranteedWin(gpa, memo);
             try memo.remember(field, w);
             if (!w) return true;
         }
@@ -145,8 +155,9 @@ fn parseArgs(gpa: Allocator) ![2]u32 {
 }
 
 pub fn main() !void {
-    var dbg = std.heap.DebugAllocator(.{}).init;
-    const gpa = dbg.allocator();
+    //var dbg = std.heap.DebugAllocator(.{}).init;
+    //const gpa = dbg.allocator();
+    const gpa = std.heap.smp_allocator;
 
     const width, const height = parseArgs(gpa) catch |err| switch (err) {
         error.ArgsExhaustion => @panic("Expected at least 2 arguments"),
@@ -159,5 +170,13 @@ pub fn main() !void {
     defer memo.deinit();
 
     const wins = try Field.analyze(gpa, &memo, width, height);
-    wins.print();
+    defer gpa.free(wins);
+
+    for (0..height) |h| {
+        for (0..width) |w| {
+            std.debug.print("{} ", .{@intFromBool(wins[w+h*width])});
+        }
+
+        std.debug.print("\n", .{});
+    }
 }
